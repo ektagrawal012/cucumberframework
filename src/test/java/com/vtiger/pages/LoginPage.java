4c2d41721447f131a20895a6d554c0cdcf0b78fa
ektagrawal012 <ektagrawal012@gmail.com>
2023-09-03 14:36:43 +0530

    xyz

--- src/test/java/com/vtiger/pages/LoginPage.java
+++ src/test/java/com/vtiger/pages/LoginPage.java
@@ -47,7 +47,8 @@ public class LoginPage extends CommonActions {
 	
 	
 	public void login(String uid, String pass)
-	{	
+	{
+		System.out.println("Login started");
 		SetInput(userid, uid,uid+" has been entered into username field");
 		SetInput(pwd, pass,pass+" has been entered into password field");
 		clickElement(login,"Login button clicked");
